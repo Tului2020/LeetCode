4c765278bf48168ba888a441fac3698e8383c81c
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-28 15:10:48 -0700

    got problem 3 working

--- Algorithms/longestsubstringnorep.js
+++ Algorithms/longestsubstringnorep.js
@@ -1,5 +1,22 @@
 const lengthOfLongestSubstring = function(s) {
-  
-  
-  
+  let longestSubString = "";
+  let longestCount = 0;
+
+  s.split('').forEach(char => {
+    let indexOfChar = longestSubString.indexOf(char)
+
+    if (indexOfChar > -1) {
+      if (longestCount < longestSubString.length) longestCount = longestSubString.length
+      longestSubString = longestSubString.slice(indexOfChar + 1)
+    }
+    longestSubString += char
+
+  })
+
+  return longestCount < longestSubString.length ? longestSubString.length : longestCount
+
 };
+
+// console.log(lengthOfLongestSubstring('abcabcbb'))
+// console.log(lengthOfLongestSubstring('bbbbb'))
+console.log(lengthOfLongestSubstring('cdd'))
