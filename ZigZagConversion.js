a2fb47debddd76e38b9c2a4db125188d16278032
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-14 20:58:35 -0700

    switching to desktop

---
+++ ZigZagConversion.js
@@ -0,0 +1,16 @@
+let input1 = "PAYPALISHIRING";
+let numRows1 = 3;
+
+const zigZagConverter = (inputString, numRows) => {
+  let returnArr = new Array(numRows).fill("");
+  let direction = 1;
+
+  for (let idx = 0; idx < inputString.length; idx++) {
+    if (idx % )
+  }
+  
+  return cycle
+}
+
+
+console.log(zigZagConverter(input1, numRows1))
